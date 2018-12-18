4697caa067464ce662980effeffd31cc1a3e8d60
sahajdhingra <sahajdhingra26@gmail.com>
2018-12-18 12:13:45 +1100

    Graphs

---
+++ Graphs/Graph_DFS.java
@@ -0,0 +1,69 @@
+// DFS traversal for a given graph
+import java.io.*;
+import java.util.*;
+class Graph_DFS
+{
+    private int V;   // No. of vertices
+ 
+    // Array  of lists for Adjacency List Representation
+    private LinkedList<Integer> adj[];
+ 
+    Graph_DFS(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+ 
+    //Function to add an edge into the graph
+    void addEdge(int v, int w)
+    {
+        adj[v].add(w);  // Add w to v's list.
+    }
+ 
+
+    void DFS_Helper(int v,boolean visited[])
+    {
+        // Mark the current node as visited and print it
+        visited[v] = true;
+        System.out.print(v+" ");
+ 
+        // Recur for all the vertices adjacent to this vertex
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext())
+        {
+            int n = i.next();
+            if (!visited[n])
+                DFS_Helper(n, visited);
+        }
+    }
+ 
+    // The function to do DFS traversal
+    void DFS(int v)
+    {
+        // Mark all the vertices as not visited(set as
+        // false by default in java)
+        boolean visited[] = new boolean[V];
+ 
+        // Call the recursive helper function to print DFS traversal
+        DFS_Helper(v, visited);
+    }
+ 
+    public static void main(String args[])
+    {
+        Graph_DFS g = new Graph_DFS(4);
+ 
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+ 
+        System.out.println("Depth First Traversal "+
+                           "(starting from vertex 2)");
+ 
+        g.DFS(2);
+    }
+}
